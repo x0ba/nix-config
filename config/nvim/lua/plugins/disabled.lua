a15056af0a2065ed56911e78cc17ee19aac06a96
Daniel Xu <heavydenial@proton.me>
2023-07-14 09:09:17 -0700

    Removed noice

--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   -- { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  -- { "folke/noice.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
   -- { "nvim-lualine/lualine.nvim", enabled = false },
 }
