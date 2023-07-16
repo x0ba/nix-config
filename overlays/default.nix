19ead3fa7be227db5e3ec7407fc660ac4c5e7a1d
Daniel Xu <heavydenial@proton.me>
2023-07-15 23:15:31 -0700

    fix: removed nekowinston-nur

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,18 +3,6 @@
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs {pkgs = final;};
 
-  overlays = [
-    (final: prev: {
-      nur = import inputs.nur {
-        nurpkgs = prev;
-        pkgs = prev;
-        repoOverrides = {
-          nekowinston = import inputs.nekowinston-nur {inherit (prev) pkgs;};
-        };
-      };
-    })
-  ];
-
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
