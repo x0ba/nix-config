65c8231a030c9b0f02c7df927497ebbb9d52b788
x0ba <hey@aspectsides.site>
2023-09-20 23:24:31 -0700

    feat: removed some fonts

--- home/apps/fonts.nix
+++ home/apps/fonts.nix
@@ -20,6 +20,7 @@
   home.packages = with pkgs; [
     (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly" "Iosevka" "JetBrainsMono"];})
     victor-mono
+    ankacoder
     ibm-plex
     maple-mono
     dosis
