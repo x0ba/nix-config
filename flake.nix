0fa98779347701652f3d445c14f39c613c81c92c
x0ba <dax@omg.lol>
2023-10-01 08:25:19 -0700

    feat: added conform.nvim

--- flake.nix
+++ flake.nix
@@ -74,7 +74,6 @@
           settings.hooks = {
             alejandra.enable = true;
             luacheck.enable = true;
-            stylua.enable = true;
             editorconfig-checker.enable = true;
             commitizen.enable = true;
             nil.enable = true;
