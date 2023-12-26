1bff8b1d81a95d394dd18fa78ef186e432113e13
x0ba <dax@omg.lol>
2023-12-25 19:06:43 -0800

    style: switch to rose-pine

--- home/apps/nvim/lua/plugins/colorscheme.lua
+++ home/apps/nvim/lua/plugins/colorscheme.lua
@@ -1,48 +1,61 @@
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   lazy = true,
-  --   name = "catppuccin",
-  --   opts = {
-  --     integrations = {
-  --       aerial = true,
-  --       alpha = true,
-  --       cmp = true,
-  --       dashboard = true,
-  --       flash = true,
-  --       gitsigns = true,
-  --       headlines = true,
-  --       illuminate = true,
-  --       indent_blankline = { enabled = true },
-  --       leap = true,
-  --       lsp_trouble = true,
-  --       mason = true,
-  --       markdown = true,
-  --       mini = true,
-  --       native_lsp = {
-  --         enabled = true,
-  --         underlines = {
-  --           errors = { "undercurl" },
-  --           hints = { "undercurl" },
-  --           warnings = { "undercurl" },
-  --           information = { "undercurl" },
-  --         },
-  --       },
-  --       navic = { enabled = true, custom_bg = "lualine" },
-  --       neotest = true,
-  --       neogit = true,
-  --       neotree = true,
-  --       noice = true,
-  --       notify = true,
-  --       semantic_tokens = true,
-  --       telescope = true,
-  --       treesitter = true,
-  --       treesitter_context = true,
-  --       which_key = true,
-  --     },
-  --   },
-  -- },
-  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        variant = "auto",
+        dark_variant = "main",
+        bold_vert_split = false,
+        dim_nc_background = false,
+        disable_background = false,
+        disable_float_background = false,
+        disable_italics = false,
+
+        groups = {
+          background = "base",
+          background_nc = "_experimental_nc",
+          panel = "surface",
+          panel_nc = "base",
+          border = "highlight_med",
+          comment = "muted",
+          link = "iris",
+          punctuation = "subtle",
+
+          error = "love",
+          hint = "iris",
+          info = "foam",
+          warn = "gold",
+
+          headings = {
+            h1 = "iris",
+            h2 = "foam",
+            h3 = "rose",
+            h4 = "gold",
+            h5 = "pine",
+            h6 = "foam",
+          },
+          -- or set all headings at once
+          -- headings = 'subtle'
+        },
+
+        -- Change specific vim highlight groups
+        -- https://github.com/rose-pine/neovim/wiki/Recipes
+        highlight_groups = {
+          ColorColumn = { bg = "rose" },
+
+          -- Blend colours against the "base" background
+          CursorLine = { bg = "foam", blend = 10 },
+          StatusLine = { fg = "love", bg = "love", blend = 10 },
+
+          -- By default each group adds to the existing config.
+          -- If you only want to set what is written in this config exactly,
+          -- you can set the inherit option:
+          Search = { bg = "gold", inherit = false },
+        },
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
