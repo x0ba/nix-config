e4d6874cb2fcb08befefe691443ad2969d11d4ad
Daniel Xu <64868985+x0ba@users.noreply.github.com>
2025-10-14 20:39:04 -0700

    init

---
+++ modules/darwin/brew.nix
@@ -0,0 +1,32 @@
+_: 
+
+{
+    homebrew = {
+        enable = true;
+        casks = [
+            "1password"
+            "anki"
+            "arc"
+            "arduino-ide"
+            "balenaetcher"
+            "claude"
+            "discord"
+            "ghostty"
+            "iina"
+            "istat-menus"
+            "jetbrains-toolbox"
+            "jordanbaird-ice"
+            "keka"
+            "logi-options+"
+            "notion"
+            "notion-calendar"
+            "obsidian"
+            "orbstack"
+            "rectangle"
+            "visual-studio-code"
+            "vivaldi"
+            "zed"
+            "zoom"
+        ];
+    };
+}
---
+++ modules/shared/packages.nix
@@ -0,0 +1,62 @@
+{ pkgs }:
+
+with pkgs; [
+  # General packages for development and system management
+  bash-completion
+  bat
+  btop
+  coreutils
+  killall
+  openssh
+  sqlite
+  wget
+  zip
+
+  # Encryption and security tools
+  age
+  age-plugin-yubikey
+  gnupg
+  libfido2
+
+  # Media-related packages
+  emacs-all-the-icons-fonts
+  dejavu_fonts
+  ffmpeg
+  fd
+  font-awesome
+  hack-font
+  noto-fonts
+  noto-fonts-emoji
+  meslo-lgs-nf
+
+  # Text and terminal utilities
+  htop
+  jetbrains-mono
+  jq
+  ripgrep
+  tree
+  tmux
+  unrar
+  unzip
+  zsh-powerlevel10k
+  
+  # Development tools
+  curl
+  gh
+  terraform
+  kubectl
+  awscli2
+  lazygit
+  fzf
+  direnv
+  
+  # Programming languages and runtimes
+  go
+  rustc
+  cargo
+  jdk11
+
+  # Python packages
+  python3
+  virtualenv
+]
